89aaac14c5fab842ba8383ba9f82818cb3741490
IPashkaYouNot <51509642+IPashkaYouNot@users.noreply.github.com>
2022-06-23 00:01:41 +0300

    Lab05 files upload

---
+++ Lab05/Program.cs
@@ -0,0 +1,39 @@
+﻿using Lab05;
+using Lab05.Logic;
+
+var registry = new OrdersRegistry();
+
+registry.AddOrder(new Order { Name = "  ", Surname = "Pavlichenko" });
+registry.AddOrder(new Order { Name = null, Surname = "Pavlichenko" });
+registry.AddOrder(new Order { Name = "Pasha", Surname = "Pavlichenko" });
+registry.AddOrder(new Order { Name = "Pasha123", Surname = "Pavlichenko" });
+
+foreach (var order in registry.GetAllOrders())
+{
+    Console.WriteLine(order);
+}
+Console.WriteLine();
+
+registry.ConsiderAll();
+
+foreach (var order in registry.GetAllOrders())
+{
+    Console.WriteLine(order);
+}
+Console.WriteLine();
+
+registry.ClearFinishedOrders();
+
+foreach (var order in registry.GetAllOrders())
+{
+    Console.WriteLine(order);
+}
+Console.WriteLine();
+
+registry.ConsiderAll();
+
+foreach (var order in registry.GetAllOrders())
+{
+    Console.WriteLine(order);
+}
+Console.WriteLine();
